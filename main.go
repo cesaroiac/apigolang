a605fb99c0b5d1c7dcce18a95a4d09b4d9133d29
Caio César <cesaroiac25@gmail.com>
2025-05-20 22:07:19 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"github.com/cesaroiac/apigolang.git/config"
 	"github.com/cesaroiac/apigolang.git/router"
 )
 
+var (
+	logger config.Logger
+)
+
 func main() {
-	
+	logger = *config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil{
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	//Initialize Router
 	router.Initialize()
 }
